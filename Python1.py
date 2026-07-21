d50734550397a17c3360833becd7b7abdd0df116
AdamGit2025 <adam.lopez.c@gmail.com>
2026-07-20 20:42:22 -0400

    El primer commit

---
+++ Python1.py
@@ -0,0 +1,7 @@
+# estos son comentarios y no afectan la programacion 
+# para abrir un comentario se realiza '#'
+# variables, se guardan en palabras 
+nombre = "Adam"
+# print es para mostrar lo que se necesita configurar 
+print("hi universe, aqui estamos configurando todo by")
+print(nombre)
